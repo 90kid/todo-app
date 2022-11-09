2b5a75f60baade8f022dafaf9f66261a3925f5b3
90kid <januszakahipis@wp.pl>
2022-11-09 17:05:37 +0100

    install pagination package

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
